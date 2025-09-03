9b343d60fe432a45952ad5c4f90e3d19694fd005
Guna Sekhar Siddabathuni <164972346+GunaSekhar9121@users.noreply.github.com>
2025-09-03 11:47:49 -0500

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,8 +16,8 @@ session = cnx.session()
 my_data_frame = session.table("SMOOTHIES.PUBLIC.FRUIT_OPTIONS") \
                       .select(col("FRUIT_NAME"), col("SEARCH_ON"))
 
-st.dataframe(data = my_data_frame,use_container_width= True)
-st.stop()
+#st.dataframe(data = my_data_frame,use_container_width= True)
+#st.stop()
 
 title = st.text_input('Name on Smoothiee')
 st.write('The name on Smoothiee will be',title)
@@ -31,6 +31,8 @@ if ingredients_list:
     ingredients_string =''
     for fruit_choosen in ingredients_list:
         ingredients_string+=fruit_choosen + ' '
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         st.subheader(fruit_choosen + ' Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_choosen)
         sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
